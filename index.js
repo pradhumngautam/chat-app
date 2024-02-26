386e600682ff1fc0004d25dc02a72c145b111c2d
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-02-26 23:11:35 +0530

    feat: script added

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ io.on('connection', (socket) => {
     console.log('message: ' + msg);
   });
 });
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
 
 server.listen(3000, () => {
   console.log("server running at http://localhost:3000");
