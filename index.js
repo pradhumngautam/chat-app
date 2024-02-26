cad3b2f528db78b2fe6d9d153548f6ec5072bb6f
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-02-26 22:36:37 +0530

    feat: script added

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const http = require("http");
 const path = require("path")
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static(path.resolve("./public")));
 
 app.get("/", (req, res) => {
   return res.sendFile("/public/index.html");
 });
-
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 server.listen(3000, () => {
   console.log("server running at http://localhost:3000");
