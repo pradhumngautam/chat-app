a46f787003c54dca093df7b3d213deebe521a6de
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-03-08 22:53:54 +0530

    Fix : socket

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ io.on('connection', (socket) => {
 });
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    io.emit('chat message', msg);\
   });
 });
 
